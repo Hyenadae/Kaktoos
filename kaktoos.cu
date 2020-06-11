c812b9f246fddacd191bcbf87ed2f080cbd3b5ad
DutChen18 <dutchen18@gmail.com>
2020-06-11 18:47:04 +0200

    lcg caching

--- kaktoos.cu
+++ kaktoos.cu
@@ -23,6 +23,10 @@
 #define WORK_UNIT_SIZE (1ULL << 23ULL)
 #endif
 
+#ifndef WORK_RANDOM_SIZE
+#define WORK_RANDOM_SIZE (WORK_UNIT_SIZE + 1000)
+#endif
+
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE 1024ULL
 #endif
@@ -39,6 +43,7 @@
 #define END (1ULL << 48ULL)
 #endif
 
+
 __device__ inline int8_t extract(const int8_t heightMap[], uint32_t i) {
     return (int8_t) (heightMap[i >> 1ULL] >> ((i & 1ULL) << 2ULL)) & 0xF;
 }
@@ -50,36 +55,69 @@ __device__ inline void increase(int8_t heightMap[], uint32_t i) {
 namespace java_random {
 
     // Random::next(bits)
-    __device__ inline uint32_t next(uint64_t *random, int32_t bits) {
-        *random = (*random * RANDOM_MULTIPLIER + RANDOM_ADDEND) & RANDOM_MASK;
-        return (uint32_t) (*random >> (48ULL - bits));
+    __device__ inline uint32_t next(uint64_t *random, size_t *i, int32_t bits) {
+        return (uint32_t) (random[++*i] >> (48ULL - bits));
     }
 
-    __device__ inline int32_t next_int_unknown(uint64_t *seed, int16_t bound) {
+    __device__ inline int32_t next_int_unknown(uint64_t *random, size_t *i, int16_t bound) {
         if ((bound & -bound) == bound) {
-            *seed = (*seed * RANDOM_MULTIPLIER + RANDOM_ADDEND) & RANDOM_MASK;
-            return (int32_t) ((bound * (*seed >> 17ULL)) >> 31ULL);
+            return (int32_t) ((bound * (random[++*i] >> 17ULL)) >> 31ULL);
         }
 
         int32_t bits, value;
         do {
-            *seed = (*seed * RANDOM_MULTIPLIER + RANDOM_ADDEND) & RANDOM_MASK;
-            bits = *seed >> 17ULL;
+            bits = random[++*i] >> 17ULL;
             value = bits % bound;
         } while (bits - value + (bound - 1) < 0);
         return value;
     }
 
     // Random::nextInt(bound)
-    __device__ inline uint32_t next_int(uint64_t *random) {
-        return java_random::next(random, 31) % 3;
+    __device__ inline uint32_t next_int(uint64_t *random, size_t *i) {
+        return java_random::next(random, i, 31) % 3;
     }
 
 }
 
-__global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int32_t *num_seeds, uint64_t *seeds) {
-    uint64_t originalSeed = blockIdx.x * blockDim.x + threadIdx.x + seed_offset;
-    uint64_t seed = originalSeed;
+struct rand_params {
+	uint64_t multiplier;
+	uint64_t addend;
+};
+
+__host__ uint64_t get_start_seed(uint64_t offset) {
+	uint64_t seed = 0;
+	uint64_t i;
+	for (i = 0; i < offset; i += 1024 * 1024) {
+		seed = (seed * 280364631195649ULL + 215216710221824ULL) & RANDOM_MASK;
+	}
+	for (i -= 1024 * 1024; i < offset; i++) {
+		seed = (seed * RANDOM_MULTIPLIER + RANDOM_ADDEND) & RANDOM_MASK;
+	}
+	return seed;
+}
+
+__host__ void gen_rand_params(rand_params *rp, size_t n) {
+	rp[0].multiplier = 1;
+	rp[0].addend = 0;
+	for (size_t i = 1; i < n; i++) {
+		rp[i].multiplier = (rp[i - 1].multiplier * RANDOM_MULTIPLIER) & RANDOM_MASK;
+		rp[i].addend = (rp[i - 1].addend * RANDOM_MULTIPLIER + RANDOM_ADDEND) & RANDOM_MASK;
+	}
+}
+
+// TODO: generate low bits seperately so crack() only has to index 32 bit values, ~3% speed improvement
+__global__ __launch_bounds__(BLOCK_SIZE, 2) void init(rand_params *rp, uint64_t *random, uint64_t seed) {
+	size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+	size_t stride = blockDim.x * gridDim.x;
+	for (size_t i = index; i < WORK_RANDOM_SIZE; i += stride) {
+		seed = (seed * rp[i].multiplier + rp[i].addend) & RANDOM_MASK;
+		random[i] = seed;
+	}
+}
+
+__global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t *random, int32_t *num_seeds, uint64_t *seeds) {
+    size_t seedIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	uint64_t originalSeed = random[seedIndex];
 
     int8_t heightMap[512];
 
@@ -101,23 +139,23 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
         if (WANTED_CACTUS_HEIGHT - extract(heightMap, currentHighestPos) > 9 * (10 - i))
             return;
 
-        initialPosX = java_random::next(&seed, 4) + 8;
-        initialPosZ = java_random::next(&seed, 4) + 8;
+        initialPosX = java_random::next(random, &seedIndex, 4) + 8;
+        initialPosZ = java_random::next(random, &seedIndex, 4) + 8;
         terrainHeight = (extract(heightMap, initialPosX + initialPosZ * 32) + FLOOR_LEVEL + 1) * 2;
 
-        initialPosY = java_random::next_int_unknown(&seed, terrainHeight);
+        initialPosY = java_random::next_int_unknown(random, &seedIndex, terrainHeight);
 
         for (a = 0; a < 10; a++) {
-            posX = initialPosX + java_random::next(&seed, 3) - java_random::next(&seed, 3);
-            posY = initialPosY + java_random::next(&seed, 2) - java_random::next(&seed, 2);
-            posZ = initialPosZ + java_random::next(&seed, 3) - java_random::next(&seed, 3);
+            posX = initialPosX + java_random::next(random, &seedIndex, 3) - java_random::next(random, &seedIndex, 3);
+            posY = initialPosY + java_random::next(random, &seedIndex, 2) - java_random::next(random, &seedIndex, 2);
+            posZ = initialPosZ + java_random::next(random, &seedIndex, 3) - java_random::next(random, &seedIndex, 3);
 
             posMap = posX + posZ * 32;
             // Keep
             if (posY <= extract(heightMap, posMap) + FLOOR_LEVEL && posY >= 0)
                 continue;
 
-            offset = 1 + java_random::next_int_unknown(&seed, java_random::next_int(&seed) + 1);
+            offset = 1 + java_random::next_int_unknown(random, &seedIndex, java_random::next_int(random, &seedIndex) + 1);
 
             for (j = 0; j < offset; j++) {
                 if ((posY + j - 1) > extract(heightMap, posMap) + FLOOR_LEVEL || posY < 0) continue;
@@ -145,31 +183,41 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
 struct GPU_Node {
     int *num_seeds;
     uint64_t *seeds;
+	uint64_t *random;
+	rand_params *rp;
 };
 
 void setup_gpu_node(GPU_Node *node, int32_t gpu) {
     cudaSetDevice(gpu);
     cudaMallocManaged(&node->num_seeds, sizeof(*node->num_seeds));
     cudaMallocManaged(&node->seeds, 1ULL << 10ULL);
+    cudaMallocManaged(&node->random, WORK_RANDOM_SIZE * sizeof(*node->random));
+    cudaMallocManaged(&node->rp, WORK_RANDOM_SIZE * sizeof(*node->rp));
 }
 
 GPU_Node nodes[GPU_COUNT];
 uint64_t offset = OFFSET;
+uint64_t seed = get_start_seed(OFFSET);
 uint64_t count = 0;
 std::mutex info_lock;
 std::vector<uint64_t> seeds;
+rand_params *rp;
 
 void gpu_manager(int32_t gpu_index) {
     std::string fileName = "kaktoos_seeds" + std::to_string(gpu_index) + ".txt";
     FILE *out_file = fopen(fileName.c_str(), "w");
-    cudaSetDevice(gpu_index);
+	cudaSetDevice(gpu_index);
+	for (size_t i = 0; i < WORK_RANDOM_SIZE; i++)
+		nodes[gpu_index].rp[i] = rp[i];
     while (offset < END) {
         *nodes[gpu_index].num_seeds = 0;
-        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>>(offset, nodes[gpu_index].num_seeds,
-                                                              nodes[gpu_index].seeds);
+        init<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>>(nodes[gpu_index].rp, nodes[gpu_index].random, seed);
         info_lock.lock();
-        offset += WORK_UNIT_SIZE;
+		seed = (seed * rp[WORK_UNIT_SIZE].multiplier + rp[WORK_UNIT_SIZE].addend) & RANDOM_MASK;
+		offset += WORK_UNIT_SIZE;
         info_lock.unlock();
+        cudaDeviceSynchronize();
+        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>>(nodes[gpu_index].random, nodes[gpu_index].num_seeds, nodes[gpu_index].seeds);
         cudaDeviceSynchronize();
         for (int32_t i = 0, e = *nodes[gpu_index].num_seeds; i < e; i++) {
             fprintf(out_file, "%lld\n", (long long int) nodes[gpu_index].seeds[i]);
@@ -183,9 +231,12 @@ void gpu_manager(int32_t gpu_index) {
     fclose(out_file);
 }
 
-int main() {
-    printf("Searching %ld total seeds...\n", (long int) (END - OFFSET));
 
+int main() {
+	printf("Searching %ld total seeds...\n", (long int) (END - OFFSET));
+	
+	rp = new rand_params[WORK_RANDOM_SIZE];
+	gen_rand_params(rp, WORK_RANDOM_SIZE);
     std::thread threads[GPU_COUNT];
 
     time_t startTime = time(nullptr), currentTime;
@@ -224,5 +275,4 @@ int main() {
 
     printf("Done!\n");
     printf("But, verily, it be the nature of dreams to end.\n");
-
 }
